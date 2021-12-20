68e8398d6325cb6264ea69b85b761c42fbd8780f
Volcano <valcano103@gmail.com>
2021-12-20 17:57:15 +0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { StrictMode } from "react"
+import ReactDOM from "react-dom"
+import "react-colorful/dist/index.css"
+import "./styles.css"
+import App from "./App"
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+)
+
